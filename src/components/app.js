d85bf28b6095d5a8d8cc8c741faf4e585493d5f8
christopherlowman <speedthrill74@gmail.com>
2022-01-20 19:10:34 -0700

    added footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from "./header"
+import Footer from "./footer"
 
 export default class App extends Component {
   render() {
---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function footer(props) {
+    return (
+        <div>
+           <h3>{props.year}</h3>
+        </div>)
+}
