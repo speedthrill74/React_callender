be654a5b151a73c1e273ca2f0ff546a805842538
christopherlowman <speedthrill74@gmail.com>
2022-01-20 19:31:42 -0700

    added dummy file

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 import Header from "./header"
 import Footer from "./footer"
 
+import DummyData from "../../static/assets/dummyData"
+
 export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monthData: dummyData.data[0]
+    }
+  }
+
   render() {
     return (
       <div className='page-wrapper'>
-        <header month="January" />
+        <Header month={this.state.monthData.name} />
+        <Footer year={this.state.monthData.year} />
       </div>
     );
   }
---
+++ static/assets/dummyData.js
@@ -0,0 +1,102 @@
+export default {
+}
+{
+    "data": [
+        {
+            "days_in_month": 31,
+            "days_in_previous_month": 31,
+            "id": 1,
+            "name": "January",
+            "start_day": 6,
+            "year": 2022
+        },
+        {
+            "days_in_month": 28,
+            "days_in_previous_month": 31,
+            "id": 4,
+            "name": "February",
+            "start_day": 2,
+            "year": 2022
+        },
+        {
+            "days_in_month": 31,
+            "days_in_previous_month": 28,
+            "id": 5,
+            "name": "March",
+            "start_day": 2,
+            "year": 2022
+        },
+        {
+            "days_in_month": 30,
+            "days_in_previous_month": 31,
+            "id": 6,
+            "name": "April",
+            "start_day": 5,
+            "year": 2022
+        },
+        {
+            "days_in_month": 31,
+            "days_in_previous_month": 30,
+            "id": 7,
+            "name": "May",
+            "start_day": 0,
+            "year": 2022
+        },
+        {
+            "days_in_month": 30,
+            "days_in_previous_month": 31,
+            "id": 8,
+            "name": "June",
+            "start_day": 3,
+            "year": 2022
+        },
+        {
+            "days_in_month": 31,
+            "days_in_previous_month": 30,
+            "id": 9,
+            "name": "July",
+            "start_day": 5,
+            "year": 2022
+        },
+        {
+            "days_in_month": 31,
+            "days_in_previous_month": 31,
+            "id": 10,
+            "name": "August",
+            "start_day": 1,
+            "year": 2022
+        },
+        {
+            "days_in_month": 30,
+            "days_in_previous_month": 31,
+            "id": 11,
+            "name": "September",
+            "start_day": 4,
+            "year": 2022
+        },
+        {
+            "days_in_month": 31,
+            "days_in_previous_month": 30,
+            "id": 12,
+            "name": "October",
+            "start_day": 6,
+            "year": 2022
+        },
+        {
+            "days_in_month": 30,
+            "days_in_previous_month": 31,
+            "id": 13,
+            "name": "November",
+            "start_day": 2,
+            "year": 2022
+        },
+        {
+            "days_in_month": 31,
+            "days_in_previous_month": 30,
+            "id": 14,
+            "name": "December",
+            "start_day": 4,
+            "year": 2022
+        }
+    ]
+}
