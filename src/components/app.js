726d5e1b5150994e9a7aebc45dc15edd2cc261b2
christopherlowman <speedthrill74@gmail.com>
2022-01-21 19:17:49 -0700

    added styles

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     super()
 
     this.state = {
-      monthData: dummyData.data[0]
+      monthData: dummyData.data[1]
     }
   }
 
--- src/components/content/contentWrapper.js
+++ src/components/content/contentWrapper.js
@@ -3,8 +3,43 @@ import DayOfWeek from "./dayOfWeek"
 import CalendarBox from "./calendarBox"
 
 export default function contentWrapper(props) {
+    const renderPreviousCalendarBoxes = () => {
+        const previousCalendarBoxes = []
+
+        for (let i=0; i<=props.startDay; i++) {
+            previousCalendarBoxes.push(
+                <CalendarBox date={props.daysInPreviousMonth - (props.startDay - i)} />
+            )
+        }
+
+        return previousCalendarBoxes
+    }
+
+    const renderCalendarBoxes = () => {
+        const calendarBoxes = []
+
+        for (let i=1; i<=props.daysInMonth; i++) {
+            calendarBoxes.push(
+                <CalendarBox date={i} />
+            )
+        }
+
+        return calendarBoxes
+    }
+
+    const renderNextCalendarBoxes = () => {
+        const nextCalendarBoxes = []
+
+        for (let i=1; i<(42 - props.daysInMonth - props.startDay); i++) {
+            nextCalendarBoxes.push(
+                <CalendarBox date={i} />
+            )
+        }
+
+        return nextCalendarBoxes
+    }
     return (
-        <div>
+        <div className="content-wrapper">
            <DayOfWeek day="Sunday" />
            <DayOfWeek day="Monday" />
            <DayOfWeek day="Tuesday" />
@@ -12,6 +47,8 @@ export default function contentWrapper(props) {
            <DayOfWeek day="Thursday" />
            <DayOfWeek day="Friday" />
            <DayOfWeek day="Saturday" />
-           <CalendarBox date="1" />
+           {renderPreviousCalendarBoxes()}
+           {renderCalendarBoxes()}
+           {renderNextCalendarBoxes()}
         </div>)
 }
