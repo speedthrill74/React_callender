78ad475dd7d52b8e4767f583b5666d494ac84897
christopherlowman <speedthrill74@gmail.com>
2022-02-07 18:47:04 -0700

    Added fetch calls to get live month data

--- src/components/app.js
+++ src/components/app.js
@@ -4,21 +4,62 @@ import Header from "./header"
 import ContentWrapper from "./content/contentWrapper"
 import Footer from "./footer"
 
-import dummyData from "../../static/assets/dummyData"
 
 export default class App extends Component {
   constructor() {
     super()
 
+    const currentDate = new Date()
+
     this.state = {
-      monthData: dummyData.data[1]
+      data: [],
+      monthData: {},
+      currentIndex: currentDate.getMonth()
     }
+
+    this.handleMonthChange = this.handleMonthChange.bind(this)
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:5000/month/get/2022")
+    .then(response => response.json())
+    .then(data => this.setState({
+      data: this.formatData(data),
+      monthData: this.formatData(data)[this.state.currentIndex]
+    }))
+    .catch(error => console.log("Error getting months: ", error))
+  }
+
+  formatData(data) {
+    const jan = data.filter(month => month.name == "January")[0]
+    const feb = data.filter(month => month.name == "February")[0]
+    const mar = data.filter(month => month.name == "March")[0]
+    const apr = data.filter(month => month.name == "April")[0]
+    const may = data.filter(month => month.name == "May")[0]
+    const jun = data.filter(month => month.name == "June")[0]
+    const jul = data.filter(month => month.name == "july")[0]
+    const aug = data.filter(month => month.name == "August")[0]
+    const sept = data.filter(month => month.name == "September")[0]
+    const oct = data.filter(month => month.name == "October")[0]
+    const nov = data.filter(month => month.name == "November")[0]
+    const dec = data.filter(month => month.name == "December")[0]
+
+    return [jan, feb, mar, apr, may, jun, jul, aug, sept, oct, nov, dec]
+  }
+
+  handleMonthChange(direction) {
+    const newIndex = direction === "next" ? this.state.currentIndex + 1 : this.state.currentIndex -1
+
+    this.setState({
+      monthData: this.state.data[newIndex],
+      currentIndex: newIndex
+    })
   }
 
   render() {
     return (
       <div className='page-wrapper'>
-        <Header month={this.state.monthData.name} />
+        <Header month={this.state.monthData.name} handleMonthChange={this.handleMonthChange} />
         <ContentWrapper 
           daysInMonth={this.state.monthData.days_in_month}
           daysInPreviousMonth={this.state.monthData.days_in_previous_month}
--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function header(props) {
     return (
         <div className="header">
-           <button>Previous</button>
+           <button onClick={() => props.handleMonthChange("previous")}>Previous</button>
            <h2>{props.month}</h2>
-           <button>Next</button>
+           <button onClick={() => props.handleMonthChange("next")}>Next</button>
         </div>
     )
 }
